5ce8423ef0a3d361a3286d2af0f153c0a6b0d1a9
Adam Brown <avb@dicebag.com>
2010-09-07 01:22:36 -0500

    Minor edits to code comments

--- Hello World/Classes/Hello_WorldAppDelegate.m
+++ Hello World/Classes/Hello_WorldAppDelegate.m
@@ -3,7 +3,7 @@
 //  Hello World
 //
 //  Created by Adam Brown on 4/13/09.
-//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//  Copyright Adam Brown 2009. All rights reserved.
 //
 
 #import "Hello_WorldAppDelegate.h"
